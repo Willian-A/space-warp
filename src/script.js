dcabc28f2f8f9a03252992324ad47ed4a468e9ba
Willian Almeida <willian.contatoal@gmail.com>
2022-03-17 15:48:16 -0300

    add nebulas

--- src/script.js
+++ src/script.js
@@ -18,11 +18,11 @@ const starColors = [
   "#ffcc6f",
 ];
 
-let LINE_COUNT = 5000;
+let LINE_COUNT = 3000;
 let geom = new THREE.BufferGeometry();
 geom.setAttribute(
   "position",
-  new THREE.BufferAttribute(new Float32Array(15000), 3)
+  new THREE.BufferAttribute(new Float32Array(8000), 3)
 );
 geom.setAttribute(
   "velocity",
@@ -36,7 +36,7 @@ let va = vel.array;
 // Init
 
 scene = new THREE.Scene();
-scene.fog = new THREE.Fog("#000", 1, 450);
+// scene.fog = new THREE.Fog("#000", 1, 450);
 // const axesHelper = new THREE.AxesHelper(5);
 // scene.add(axesHelper);
 
@@ -78,7 +78,10 @@ let mat = new THREE.LineBasicMaterial({ vertexColors: true });
 let lines = new THREE.LineSegments(geom, mat);
 scene.add(lines);
 
+// Controls
+
 // const controls = new OrbitControls(camera, canvas);
+
 window.addEventListener("resize", onWindowResize, false);
 
 function onWindowResize() {
@@ -87,11 +90,15 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
+renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  canvas: canvas,
+  alpha: true,
+});
 renderer.autoClear = false;
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
-renderer.setClearColor(0x000000, 0.0);
+// renderer.setClearColor(0x000000, 0.0);
 
 document.body.appendChild(renderer.domElement);
 
@@ -108,8 +115,8 @@ let unrealBloom = new UnrealBloomPass(
 );
 
 unrealBloom.threshold = 0;
-unrealBloom.strength = 3; //intensity of glow
-unrealBloom.radius = 0.5;
+unrealBloom.strength = 5; //intensity of glow
+unrealBloom.radius = 1;
 
 composer.setSize(window.innerWidth, window.innerHeight);
 composer.renderToScreen = true;
@@ -118,20 +125,67 @@ composer.addPass(unrealBloom);
 
 let velocity = 0.01;
 let acceleration = 0.0101;
+let nebulaAcc = 0.2;
 
 function speedUp() {
   velocity = 0.15;
   acceleration = 0.16;
+  nebulaAcc = 1;
 }
 function speedDown() {
   velocity = 0.01;
   acceleration = 0.0101;
+  nebulaAcc = 0.2;
 }
 
 const button_speedUp = document.getElementById("speed_up");
 const button_speedDown = document.getElementById("speed_down");
 button_speedUp.addEventListener("click", speedUp);
 button_speedDown.addEventListener("click", speedDown);
+
+// Cloud
+const cloudParticles = [];
+let loader = new THREE.TextureLoader();
+loader.load("textures/smoke.png", function (texture) {
+  //texture is loaded
+  const cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
+  const cloudMaterial = new THREE.MeshLambertMaterial({
+    map: texture,
+    transparent: true,
+    opacity: 0.05,
+  });
+
+  for (let i = 0; i < 70; i++) {
+    let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+    cloud.position.set(
+      Math.random() * 400 - 250,
+      Math.random() * 200 - 100,
+      Math.random() * 200 - 450
+    );
+    // cloud.rotation.x = 0.1;
+    // cloud.rotation.y = -0.12;
+    cloud.rotation.z = Math.random() * 2 * Math.PI;
+    cloudParticles.push(cloud);
+    scene.add(cloud);
+  }
+});
+
+// Lights
+let light1 = new THREE.PointLight("#cc6600", 25, 450, 1.7);
+light1.position.set(0, 0, 1);
+// const helper = new THREE.PointLightHelper(light1, 5);
+// scene.add(helper);
+scene.add(light1);
+let light2 = new THREE.PointLight("#cf0a3f", 40, 450, 1.7);
+light2.position.set(-400, 300, 70);
+// const helper2 = new THREE.PointLightHelper(light2, 5);
+// scene.add(helper2);
+scene.add(light2);
+let light3 = new THREE.PointLight("#3677ac", 50, 450, 1.7);
+light3.position.set(300, 300, 50);
+// const helper3 = new THREE.PointLightHelper(light3, 5);
+// scene.add(helper3);
+scene.add(light3);
 
 function animate() {
   for (let line_index = 0; line_index < LINE_COUNT; line_index++) {
@@ -153,6 +207,15 @@ function animate() {
       va[2 * line_index + 1] = 0;
     }
   }
+
+  cloudParticles.forEach((p) => {
+    p.position.z += nebulaAcc;
+    p.rotation.z += 0.0005;
+    if (p.position.z > 30) {
+      p.position.z = Math.random() * 200 - 600;
+    }
+    // console.log(p.rotation.z);
+  });
   pos.needsUpdate = true;
   // renderer.render(scene, camera);
   composer.render();
